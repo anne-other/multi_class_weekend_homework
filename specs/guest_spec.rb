deaea5d5657521c086ee8f3c624c3935d22086fd
anne-other <anne.c.ince@gmail.com>
2018-07-29 14:56:07 +0100

    add wallet to guest

--- specs/guest_spec.rb
+++ specs/guest_spec.rb
@@ -5,9 +5,16 @@ require_relative('../guests.rb')
 
 class GuestTest < MiniTest::Test
 
+  def setup
+    @guest = Guest.new("Anne", 50)
+  end
+
   def test_guest_has_name()
-    guest = Guest.new("Anne")
-    assert_equal("Anne", guest.name())
+    assert_equal("Anne", @guest.name())
+  end
+
+  def test_guest_has_money()
+    assert_equal(50, @guest.wallet())
   end
 
 
